980b705171315b5e345273d3d12dd9dd8ae3ee03
Daniel Andrei Minca <mandrei17@gmail.com>
2017-08-06 22:49:16 +0300

    split module utils into separate file and refactor
    * split module utils into its own file and import AnsibleModule in order
      to inject argument_spec()
    * move certain methods from module_utils into `api_key_module`
    
    Resolves:
    Related: #1
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- library/rancher_api_key.py
+++ library/rancher_api_key.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from ansible.module_utils.basic import AnsibleModule
-from time import sleep
+from ansible.module_utils.rancher import RancherModule
 
 try:
     import requests
@@ -9,95 +9,125 @@ except ImportError:
 else:
     REQUESTS_FOUND = False
 
-
-def main():
-    API_KEYS_URL = '{protocol}://{url}:{port}/v2-beta/apiKeys'.format(
-        protocol='http',
-        url='localhost',
-        port='8080'
-    )
-
-    module_args = dict(
-        name=dict(type='str', required=True),
-        description=dict(type='str', required=False, default='generic'),
-        public_key=dict(type='str', required=True),
-        secret_key=dict(type='str', required=True, no_log=True),
-        state=dict(required=True, choices=['present', 'absent'])
-    )
-
-    result = dict(
-        changed=False,
-        original_message='',
-        message=''
-    )
-
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
-
-    if module.check_mode:
-        return result
-
-
-def create_api_key(name, public_key, secret_key, description):
-    payload = dict(
-        name=name,
-        publicValue=public_key,
-        secretValue=secret_key,
-        description=description
-    )
-    try:
-        requests.post(API_KEYS_URL, data=payload)
-    except HTTPError as error:
-        module.fail_json(msg='Failed to create API key: {err}'.format(err=error), **result)
-    else:
-        module.exit_json(
-            changed=True,
-            msg='Created API Key {name}'.format(
-                name=payload.name
-            ), **result
+class RancherApiKeyModule(RancherModule):
+    """The rancher_api_key module for Ansible"""
+    def __init__(self):
+        super(RancherApiKeyModule, self).__init__(
+            module = AnsibleModule(
+                argument_spec=RancherModule.argument_spec()
+            )
         )
 
+        self.name = self.module.params['name']
+        self.description = self.module.params['description']
+        self.public_key = self.module.params['public_key']
+        self.secret_key = self.module.params['secret_key']
+        self.state = self.module.params['state']
 
-def delete_api_key(key_name):
-    removes = []
-    api_keys_get = requests.get(API_KEYS_URL)
-    for api_key in api_keys_get.json()['data']:
-        ids = api_key['id'], api_key['name']
-        removes.append(ids)
-        for i in removes:
-            if i[1] == key_name:
-                key_name = i[0]
-                try:
-                    requests.post("{url}/{key_id}?action=deactivate".format(
-                        url=API_KEYS_URL,
-                        key_id=key_name
-                    ))
-                    time.sleep(1) # needs time to deactivate
-                    requests.delete("{url}/{key_id}".format(
-                        url=API_KEYS_URL,
-                        key_id=key_name
-                    ))
-                except HTTPError as error:
-                    module.fail_json(msg='Failed to delete API key: {err}'.format(err=error), **result)
-                else:
-                    module.exit_json(
-                        changed=True,
-                        msg='Deleted API Key {name}'.format(
-                            name=key_name
-                        ), **result
-                    )
+    def state_present(self):
+        api_key = self._get_api_key()
+
+        if api_key:
+            self.module.exit_json(
+                changed=False,
+                msg='API Key already exists',
+                api_key=self._api_key_to_dict(api_key)
+            )
+
+        api_key = self._create_api_key()
+
+        self.module.exit_json(
+            changed=True,
+            msg='Created API Key {key}'.format(key=self.name),
+            api_key=self._api_key_to_dict(api_key)
+        )
+
+    def state_absent(self):
+        api_key = self._get_api_key()
+
+        if not api_key:
+            self.module.exit_json(
+                changed=False,
+                msg='API Key {key} does not exist'.format(key=self.name),
+                api_key=self._api_key_to_dict(api_key)
+            )
+
+            # TODO: do we really need this?
+            return
+
+        self._delete_api_key(api_key)
 
 
-def get_api_key(search):
-    api_keys = requests.get(API_KEYS_URL).json()['data']
-    matched_key = [api_key for api_key in api_keys if api_key['name'] == search]
-    if matched_key:
-        return matched_key[0]
+    def _api_key_to_dict(self, api_key):
+        api_key_dict = dict(
+            id=api_key['id'],
+            name=api_key['name'],
+            description=api_key['description']
+        )
 
-    return None
+        return api_key_dict
 
+    def _create_api_key(self):
+        payload = dict(
+            name=self.name,
+            publicValue=self.public_key,
+            secretValue=self.secret_key,
+            description=self.description
+        )
+        try:
+            requests.post(API_KEYS_URL, data=payload)
+        except HTTPError as error:
+            module.fail_json(msg='Failed to create API key: {err}'.format(err=error), **result)
+        else:
+            module.exit_json(
+                changed=True,
+                msg='Created API Key {name}'.format(
+                    name=payload.name
+                ), **result
+            )
+
+    def _delete_api_key(self, api_key):
+        removes = []
+        api_keys_get = requests.get(API_KEYS_URL)
+        for api_key in api_keys_get.json()['data']:
+            ids = self.id, self.name
+            removes.append(ids)
+            for i in removes:
+                if i[1] == api_key:
+                    api_key = i[0]
+                    try:
+                        requests.post("{url}/{key_id}?action=deactivate".format(
+                            url=API_KEYS_URL,
+                            key_id=api_key
+                        ))
+                        time.sleep(1) # needs time to deactivate
+                        requests.delete("{url}/{key_id}".format(
+                            url=API_KEYS_URL,
+                            key_id=api_key
+                        ))
+                    except HTTPError as error:
+                        module.fail_json(
+                            msg='Failed to delete API key: {err}'.format(
+                                err=error
+                            ), **result)
+                    else:
+                        module.exit_json(
+                            changed=True,
+                            msg='Deleted API Key {name}'.format(
+                                name=api_key
+                            ), **result
+                        )
+
+    def _get_api_key(self):
+        api_keys = requests.get(API_KEYS_URL).json()['data']
+        matched_key = [api_key for api_key in api_keys if api_key['name'] == self.name]
+        if matched_key:
+            return matched_key[0]
+
+        return None
+
+def main():
+    module = RancherApiKeyModule()
 
 if __name__ == '__main__':
     main()
---
+++ module_utils/rancher.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+from ansible.module_utils.basic import AnsibleModule
+from time import sleep
+try:
+    import requests
+except ImportError:
+    REQUESTS_FOUND = True
+else:
+    REQUESTS_FOUND = False
+
+DEFAULT_RANCHER_HOST = 'http://localhost:8080'
+API_KEYS_URL = '{host}/v2-beta/apiKeys'.format(host=DEFAULT_RANCHER_HOST)
+
+class RancherModule(object):
+    """Base class containing utilities for the Rancher Module"""
+    def __init__(self, module):
+        """
+        Create a new RancherModule
+
+        Will fail if requests module is not present.
+        :param module: The underlying Ansible module
+        :type module: AnsibleModule
+        """
+        self.module = module
+
+        if not REQUESTS_FOUND:
+            self.module.fail_json(msg='requests is required for this module.')
+            # TODO: do we really need this?
+            return
+
+    @staticmethod
+    def argument_spec(**additional_arg_spec):
+        spec = dict(
+            name=dict(type='str', required=True),
+            description=dict(type='str', required=False, default='generic'),
+            public_key=dict(type='str', required=True),
+            secret_key=dict(type='str', required=True, no_log=True),
+            state=dict(required=True, choices=['present', 'absent'])
+        )
+
+        if additional_arg_spec:
+            spec.update(additional_arg_spec)
+
+        return spec
