526475a142be67a4a918354c33f08511013822d9
Daniel Andrei Minca <mandrei17@gmail.com>
2017-08-08 12:47:46 +0300

    state present is working key is deployed
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- library/rancher_api_key.py
+++ library/rancher_api_key.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
-from ansible.module_utils.basic import AnsibleModule
-from .module_utils.rancher.rancher import RancherModule
 
+from __future__ import (absolute_import, division)
+__metaclass__ = type
+
+from ansible.module_utils.basic import AnsibleModule
 import requests
 
-class RancherApiKeyModule(RancherModule):
-    """The rancher_api_key module for Ansible"""
+DEFAULT_RANCHER_HOST = 'http://localhost:8080'
+API_KEYS_URL = '{host}/v2-beta/apiKeys'.format(host=DEFAULT_RANCHER_HOST)
+
+class RancherAPIKeyModule(object):
     def __init__(self):
-        super(RancherApiKeyModule, self).__init__(
-            module = AnsibleModule(
-                argument_spec=RancherModule.argument_spec()
-            )
+        self.module = AnsibleModule(
+            argument_spec=dict(
+                name=dict(type='str', required=True),
+                description=dict(type='str', required=False, default='generic'),
+                public_key=dict(type='str', required=True),
+                secret_key=dict(type='str', required=True, no_log=True),
+                state=dict(choices=['present', 'absent'], default='present')
+            ), supports_check_mode=True
         )
 
         self.name = self.module.params['name']
@@ -21,35 +29,28 @@ class RancherApiKeyModule(RancherModule):
 
     def state_present(self):
         api_key = self._get_api_key()
-
         if api_key:
             self.module.exit_json(
                 changed=False,
                 msg='API Key already exists',
                 api_key=self._api_key_to_dict(api_key)
             )
-
         api_key = self._create_api_key()
-
         self.module.exit_json(
             changed=True,
             msg='Created API Key {key}'.format(key=self.name),
             api_key=self._api_key_to_dict(api_key)
         )
 
+
     def state_absent(self):
         api_key = self._get_api_key()
-
         if not api_key:
             self.module.exit_json(
                 changed=False,
                 msg='API Key {key} does not exist'.format(key=self.name),
                 api_key=self._api_key_to_dict(api_key)
             )
-
-            # TODO: do we really need this?
-            return
-
         self._delete_api_key(api_key)
 
 
@@ -59,8 +60,16 @@ class RancherApiKeyModule(RancherModule):
             name=api_key['name'],
             description=api_key['description']
         )
-
         return api_key_dict
+
+
+    def _get_api_key(self):
+        api_keys = requests.get(API_KEYS_URL).json()['data']
+        matched_key = [api_key for api_key in api_keys if api_key['name'] == self.name]
+        if matched_key:
+            return matched_key[0]
+        return None
+
 
     def _create_api_key(self):
         payload = dict(
@@ -72,20 +81,21 @@ class RancherApiKeyModule(RancherModule):
         try:
             requests.post(API_KEYS_URL, data=payload)
         except HTTPError as error:
-            self.module.fail_json(msg='Failed to create API key: {err}'.format(err=error), **result)
+            self.module.fail_json(msg='Failed to create API key: {err}'.format(err=error))
         else:
             self.module.exit_json(
                 changed=True,
                 msg='Created API Key {name}'.format(
-                    name=payload.name
-                ), **result
+                    name=self.name
+                )
             )
+
 
     def _delete_api_key(self, api_key):
         removes = []
         api_keys_get = requests.get(API_KEYS_URL)
         for api_key in api_keys_get.json()['data']:
-            ids = self.id, self.name
+            ids = api_key['id'], self.name
             removes.append(ids)
             for i in removes:
                 if i[1] == api_key:
@@ -113,46 +123,14 @@ class RancherApiKeyModule(RancherModule):
                             ), **result
                         )
 
-    def _get_api_key(self):
-        api_keys = requests.get(API_KEYS_URL).json()['data']
-        matched_key = [api_key for api_key in api_keys if api_key['name'] == self.name]
-        if matched_key:
-            return matched_key[0]
-
-        return None
 
 def main():
-    DEFAULT_RANCHER_HOST = 'http://localhost:8080'
-    API_KEYS_URL = '{host}/v2-beta/apiKeys'.format(host=DEFAULT_RANCHER_HOST)
+    module = RancherAPIKeyModule()
+    if module.state == 'present':
+        module.state_present()
+    elif module.state == 'absent':
+        module.state_absent()
 
-    module = AnsibleModule(
-        argument_spec=dict(
-            name=dict(type='str', required=True),
-            description=dict(type='str', required=False, default='generic'),
-            public_key=dict(type='str', required=True),
-            secret_key=dict(type='str', required=True, no_log=True),
-            state=dict(choices=['present', 'absent'], default='present')
-        ), supports_check_mode=True
-    )
-
-    def _create_api_key(self):
-        payload = dict(
-            name=self.name,
-            publicValue=self.public_key,
-            secretValue=self.secret_key,
-            description=self.description
-        )
-        try:
-            requests.post(API_KEYS_URL, data=payload)
-        except HTTPError as error:
-            self.module.fail_json(msg='Failed to create API key: {err}'.format(err=error), **result)
-        else:
-            self.module.exit_json(
-                changed=True,
-                msg='Created API Key {name}'.format(
-                    name=payload.name
-                ), **result
-            )
 
 if __name__ == '__main__':
     main()
