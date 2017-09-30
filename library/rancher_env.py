8552357ae4381c28331d6c306c06ebb12e0ec391
Daniel Andrei Minca <mandrei17@gmail.com>
2017-09-30 18:56:27 +0300

    define values required for an environment
    Resolves:
    Related:
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- library/rancher_env.py
+++ library/rancher_env.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python
 from __future__ import (absolute_import, division)
 from ansible.module_utils.basic import AnsibleModule
+import requests
+
+__metaclass__ = type
+
+DEFAULT_RANCHER_HOST = 'http://localhost:8080'
+API_URL = '{host}/v2-beta/'.format(host=DEFAULT_RANCHER_HOST)
+
+
+class RancherProjectModule(object):
+    """Rancher Environments (or Project in Rancher API slang)
+       CRUD.
+    """
+    def __init__(self):
+        self.module = AnsibleModule(
+            argument_spec=dict(
+                name
+                description
+                members
+            )
+        )
